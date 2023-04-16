b0706faf6822374d4dad7f08ec769153a18e589b
sigttou <david@crap.solutions>
2023-04-16 19:13:47 +0200

    INIT

---
+++ reddit-create.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+import requests
+import jinja2
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+def get_datasets(url):
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, features="lxml")
+    html_table = soup.find("table")
+
+    headings = [th.get_text() for th in
+                html_table.find("tr").find_all("th")]
+    datasets = []
+    for row in html_table.find_all("tr")[1:]:
+        dataset = zip(headings,
+                      (td.get_text() for td in row.find_all("td")))
+        datasets.append(tuple(dataset))
+
+    return datasets
+
+
+def get_table(url):
+    datasets = get_datasets(url)
+    table = []
+    for ds in datasets:
+        entry = {}
+        entry['name'] = \
+            ds[2][1].split('<')[0].encode().decode("unicode_escape")
+        entry['points'] = \
+            int(ds[9][1].split('<')[0].encode().decode("unicode_escape"))
+        table.append(entry)
+
+    return table
+
+
+def get_gameplan(url):
+    datasets = get_datasets(url)
+    gameplan = []
+    for ds in datasets:
+        if not ds:
+            # just fetch current season
+            break
+        entry = {}
+        entry['date'] = ds[2][1].split('<')[0]
+        entry['time'] = ds[3][1].split('<')[0]
+        entry['home'] = \
+            ds[4][1].split('<')[0].encode().decode("unicode_escape")
+        entry['away'] = \
+            ds[5][1].split('<')[0].encode().decode("unicode_escape")
+        entry['res'] = ds[6][1].split('\\n')[0].split(' ')[0]
+        entry['league'] = bool(ds[7][1][-1].strip())
+
+        if not entry['res']:
+            entry['res'] = "-:-"
+        else:
+            a = int(entry['res'].split(':')[0])
+            b = int(entry['res'].split(':')[1])
+            res = ''
+            if a == b:
+                res = 'D'
+            elif entry['home'] == 'Grazer AK 1902':
+                res = 'W' if a > b else 'L'
+            else:
+                res = 'L' if a > b else 'W'
+            entry['res'] = res + ' (' + entry['res'] + ')'
+
+        gameplan.append(entry)
+    return gameplan
+
+
+def get_next_games(gameplan):
+    next_games = []
+    for e in gameplan:
+        dt = datetime.strptime(e['date'], "%d.%m.%Y").date()
+        now = datetime.now().date()
+        if dt >= now:
+            # show todays game too
+            next_games.append(e)
+        if len(next_games) >= 2:
+            break
+
+    return next_games
+
+
+def main():
+    jenv = jinja2.Environment(
+        loader=jinja2.FileSystemLoader("templates/"))
+    sidebar_tmpl = jenv.get_template("sidebar.tmpl")
+    gameplan_tmpl = jenv.get_template("gameplan.tmpl")
+    table_url = "https://www.2liga.at/ajax.php?contelPageId=8686" + \
+                "&file=/?proxy=daten/BL2/20222023/tabellen/tabelle" + \
+                "_gesamt_1-23.html"
+    table = get_table(table_url)
+
+    gameplan_url = "https://www.2liga.at/ajax.php?contelPageId=8686" + \
+                   "&file=/?proxy=daten/team/html/6457/spielplan.html"
+    gameplan = get_gameplan(gameplan_url)
+    next_games = get_next_games(gameplan)
+
+    content = sidebar_tmpl.render(table=table, next_games=next_games)
+    with open("output/sidebar.txt", 'w') as f:
+        f.write(content)
+
+    content = gameplan_tmpl.render(gameplan=gameplan)
+    with open("output/gameplan.txt", 'w') as f:
+        f.write(content)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ ticket-check.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import requests
+
+
+def main():
+    base_url = "https://ticket.grazerak.at/backend/events/"
+    events_ep = "futurePublishedEvents"
+
+    print("<!DOCTYPE html>")
+    print("<html>")
+    print("<body>")
+    for event in requests.get(base_url + events_ep).json():
+        print("<h1>" + event["title"] + "</h1>")
+
+        event_url = base_url + event["id"] + "/"
+        chk_url = event_url + "public-stadium-representation-config"
+
+        content = requests.get(chk_url).json()
+        sold_cnt = 0
+        avail_cnt = 0
+        for entry in content['sectorRepresentationConfigurations']:
+            for e in entry['seatConfigurations']:
+                if e['seatStatus'] == 'SOLD':
+                    sold_cnt += 1
+                if e['seatStatus'] == 'AVAILABLE':
+                    avail_cnt += 1
+
+        print("<p>Available: " + str(avail_cnt) + "</p>")
+        print("<p>Sold (w/o Sponsors, VIP): " + str(sold_cnt) + "</p>")
+        est_vip = 896  # from SKN home game, not verified
+        print("<p>Sold (w est. 896 Sponsors, VIP): " +
+              str(sold_cnt + est_vip) + "</p>")
+        print("<br>")
+
+    print("</body>")
+    print("</html>")
+    return
+
+    # 5619 (sections 15-25)
+    # 65 (VIP loge 1); likely sold out (?)
+    # 60 (VIP loge 2); likely sold out (?)
+    # 63 (VIP loge 3); likely sold out (?)
+    # 65 (VIP loge 4); likely sold out (?)
+    # 60 (VIP loge 5); likely sold out (?)
+    # -581 (sector 24); usually not selling
+    # -571 (sector 25); not selling
+    # -416 (sector 15); not selling (Sponsors?)
+    # -236 (sector 18); not selling (VIP?)
+    # -156 (sector 18 VIP); sold out (?)
+    # mx = 5619 - 581 - 571 - 416 - 236 - 156
+    # vip_cnt = 65 + 60 + 63 + 65 + 60 + 156 + 236
+
+
+if __name__ == "__main__":
+    main()
